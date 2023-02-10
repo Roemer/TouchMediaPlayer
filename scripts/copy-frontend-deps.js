f84712d4b1fac14723be9794cdb83df5fe7d3c18
Roman Baeriswyl <r.baeriswyl@gmail.com>
2023-02-10 21:09:34 +0000

    Fixed scripts

--- scripts/copy-frontend-deps.js
+++ scripts/copy-frontend-deps.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const modulesRoot = 'node_modules';
 const targetCssPath = 'frontend/css';
